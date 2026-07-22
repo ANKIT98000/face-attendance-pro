bc5d111d9c06e39a54b4e1e58e3cff00469d99fe
ANKIT98000 <anku.mahala555@gmail.com>
2026-07-22 19:19:08 +0530

    Fixed DB_PORT issue in database connection

--- database.py
+++ database.py
@@ -9,6 +9,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PORT = os.getenv("DB_PORT")
 
 def get_db_connection():
     """Establishes and returns a connection to the PostgreSQL database."""
@@ -22,6 +23,7 @@ def get_db_connection():
             database=DB_NAME,
             user=DB_USER,
             password=DB_PASSWORD
+            port=DB_PORT
         )
         return conn
     except Exception as e:
